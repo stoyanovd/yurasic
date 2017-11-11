27d5f00c21a36776ac6945b0663c6e47e2183e5c
Dmitry Stoyanov <stoyanovd@mail.ru>
2017-11-11 20:00:08 +0300

    Add HierarchyItem to models

--- songsapp/models.py
+++ songsapp/models.py
@@ -10,8 +10,25 @@ class Tag(models.Model):
 
     # I think we don't need one-letter tags
     # (and not sure that we need two-letters tags)
-    name = models.CharField(max_length=MC.MAX_NAME_LENGTH, blank=False)
-    # TODO think about synonyms
+    # name = models.CharField(max_length=MC.MAX_NAME_LENGTH, blank=False)
+    tag = models.SlugField(default="")
+    # TODO think about synonyms_namings
+
+    def __str__(self):
+        return self.tag
+
+
+class HierarchyItem(models.Model):
+    app_label = 'songsapp'
+
+    parent = models.ForeignKey('self', null=True, related_name='children')
+
+    # content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    # object_id = models.PositiveIntegerField()
+    # content_object = GenericForeignKey('content_type', 'object_id')
+    #
+    # def __str__(self):  # __unicode__ on Python 2
+    #     return self.tag
 
 
 class Author(models.Model):
@@ -19,6 +36,11 @@ class Author(models.Model):
 
     name = models.CharField(max_length=MC.MAX_NAME_LENGTH, blank=False)
     tags = models.ManyToManyField(Tag)
+
+    node = models.OneToOneField('HierarchyItem', null=True)
+
+    def __str__(self):
+        return self.name
 
     # TODO create_date maybe needed too
 
@@ -30,6 +52,11 @@ class Song(models.Model):
     # realizations one-to-many
     authors = models.ManyToManyField(Author)
     tags = models.ManyToManyField(Tag)
+
+    node = models.OneToOneField('HierarchyItem', null=True, related_name='target')
+
+    def __str__(self):
+        return self.title
 
     def any_author_name(self):
         a = self.authors.all()
@@ -45,11 +72,13 @@ class Realization(models.Model):
     app_label = 'songsapp'
 
     content = models.TextField(blank=True)
-    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    song = models.ForeignKey(Song, on_delete=models.CASCADE, related_name='realization_set')
     create_date = models.DateTimeField('date created', default=timezone.now)
     tags = models.ManyToManyField(Tag)
 
     source_url = models.URLField(blank=True)
+
+    # node = models.OneToOneField('HierarchyItem')
 
     def was_created_recently(self):
         return timezone.now() >= self.create_date >= timezone.now() - datetime.timedelta(days=1)
@@ -58,6 +87,23 @@ class Realization(models.Model):
         from django.urls import reverse
         # TODO learn how to use reverse here
         return '/songs/%i/' % self.id
+
+
+from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
+
+class TaggedItem(models.Model):
+    app_label = 'songsapp'
+
+    tag = models.SlugField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    def __str__(self):  # __unicode__ on Python 2
+        return self.tag
 
 
 class Wonder(models.Model):
--- yurasic/settings.py
+++ yurasic/settings.py
@@ -56,6 +56,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'django_extensions',
+
     'haystack',
 
     'songsapp.apps.SongsappConfig',
